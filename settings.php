e3fbdf66f4949fc714455f349339b20b8329c152
vbartels <veit@ludus-venti.de>
2021-06-09 13:53:56 +0200

    Update settings.php

--- settings.php
+++ settings.php
@@ -9,7 +9,7 @@ $apikey = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx";
 $endpoint = "https://128.koronacloud.com/web/api/v3";
 
 //KETTIKETT settings:
-$font = 'font/OfficinaSansStd-Bold.otf';
+$font = 'font/OfficinaSansStd-Bold.otf'; //Not included in github
 $schildwidth = 709; 	//60mm x 30mm @ 300ppi
 $schildheight = 354;	//60mm x 30mm @ 300ppi
 ###
